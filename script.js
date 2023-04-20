4bdedb2c3e255afe400021a768c1bc90d0ed6c0f
Lica15 <angelgala1512@gmail.com>
2023-04-20 18:07:04 +0300

    changes for icon

--- script.js
+++ script.js
@@ -58,9 +58,4 @@ searchForm.addEventListener("submit", handleSubmit);
 search("New York");
 
 let iconElement = document.querySelector("#icon");
-function displayIcon(response) {
-  iconElement.setAttribute(
-    "src",
-    `https://openweathermap.org/img/wn/10d@2x.png`
-  );
-}
+iconElement.setAttribute("src", `https://openweathermap.org/img/wn/10d@2x.png`);
