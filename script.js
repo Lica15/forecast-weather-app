7bb5e0d855d72499c121cd1e5f250119b795a2e0
Lica15 <angelgala1512@gmail.com>
2023-04-25 08:27:54 +0300

    changed api for displayForecast

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function displayForecast(response) {
   console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Thu", "Fri", "Sat", "Sun"];
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
 
   let forecastHTML = `<div class="row">`;
   days.forEach(function (day) {
@@ -57,7 +57,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "057ea9e54c5ded79de95a8a3d183341c";
+  let apiKey = "97f8e93f00107773f88eafd933ce86b7";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
